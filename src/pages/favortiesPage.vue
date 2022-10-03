ec61b2cee1b82624d80188143a02a5ca581917a9
Abhishek Juneja <darth-dodo@users.noreply.github.com>
2022-10-03 14:12:11 +0200

    List, Detail and Card Components  (#3)
    * using vuex for creating actions, mutations and adding it to pages for state management
    
    * adding pokemon card component with data call
    
    * adding pokemon list view
    
    * integrating the components with home page and favorites page
    
    * adding screenshots

--- src/pages/favortiesPage.vue
+++ src/pages/favortiesPage.vue
@@ -1,29 +1,48 @@
-/* eslint-disable *
 <template>
     <div>
-        <h1 class="overview-favorite-pokemon__title">Your favorite Pokémon</h1>
-        <router-link class="nes-btn" to="/">Go back</router-link>
-        <div class="overview-favortite-pokemon"/>
+      <h1 class="overview-favorite-pokemon__title">
+        Your favorite Pokémon
+      </h1>
+      <router-link
+        class="nes-btn"
+        to="/"
+      >
+        Go back
+      </router-link>
+      <div class="overview-favorite-pokemon">
+        <pokemon-card
+          v-for="(pokemon, index) in stateFavoritePokemonList"
+          :key="index"
+          :pokemon-name="pokemon"
+        />
+      </div>
     </div>
-</template>
-
-<script>
-import { mapState } from 'vuex'
-    export default {
-        computed: {
-            ...mapState(['stateFavoritePokemonList'])
-        }
-    }
-</script>
-
-<style scoped>
-.overview-favorite-pokemon {
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-}
-
-.overview-favorite-pokemon__title {
+  </template>
+  
+  <script>
+  import { mapState } from 'vuex'
+  import PokemonCard from '@/components/PokemonCard'
+  
+      export default {
+          components: {
+              PokemonCard
+          },
+          computed: {
+              ...mapState([
+              'stateFavoritePokemonList'
+              ])
+          }
+      }
+  </script>
+  
+  <style scoped>
+  .overview-favorite-pokemon {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+  }
+  
+  .overview-favorite-pokemon__title {
     margin-top: 10px;
     text-align: center;
-}
-</style>
+  }
+  </style>
--- src/pages/homePage.vue
+++ src/pages/homePage.vue
@@ -1,43 +1,61 @@
 <template>
     <div class="select-pokemon-page">
-        <img src="./../assets/logo-pixel.gif"
-        alt="pokémon"/>
-        <h1>Home Page</h1>
-        <div class="select-pokemon-content" />
+      <img
+        src="./../assets/logo-pixel.gif"
+        alt="pokémon"
+      >
+      <div class="select-pokemon-content">
+        <pokemon-list
+          :pokemon-list="statePokemonDataList"
+          :favorites="stateFavoritePokemonList"
+        />
+        <summary-favorites
+          :pokemon-list="statePokemonDataList"
+          :favorites="stateFavoritePokemonList"
+        />
+      </div>
     </div>
-</template>
-
-<script>
-import { mapState, mapActions } from 'vuex'
-export default {
-    computed: {
-        ...mapState(['statePokemonDataList', 'stateFavoritePokemonList']),
-    },
-    async created() {
-        const pokemonData = await this.getPokemonData()
-        this.setPokemonData(pokemonData)
-    },
-    methods: {
-        async getPokemonData() {
-            const data = await fetch(
-                'https://pokeapi.co/api/v2/pokemon?limit=150'
-            )
-            const apiResult = await data.json()
-            return apiResult.results
-        },
-        ...mapActions(['setPokemonData', 'addFavorite', 'deleteFavorite', 'eraseFavoritePokemonList']),
-    },
-}
-</script>
-
-<style scoped>
-    .select-pokemon-page {
-        text-align: center;
-    }
-
-    .select-pokemon-content {
-        text-align: left;
-        display: flex;
-        justify-content: space-evenly;
-    }
-</style>
+  </template>
+  
+  <script>
+  import PokemonList from '@/components/PokemonList'
+  import SummaryFavorites from '@/components/SummaryFavorites'
+  import { mapState, mapActions } from 'vuex'
+  
+  export default {
+      components: {
+          PokemonList,
+          SummaryFavorites,
+      },
+      computed: {
+          ...mapState(['statePokemonDataList', 'stateFavoritePokemonList']),
+      },
+      async created() {
+          const pokemonData = await this.getPokemonData()
+          this.setPokemonData(pokemonData)
+      },
+      methods: {
+          async getPokemonData() {
+              const data = await fetch(
+                  'https://pokeapi.co/api/v2/pokemon?limit=151'
+              )
+              const json = await data.json()
+              return json.results
+          },
+          ...mapActions(['setPokemonData']),
+      },
+  }
+  </script>
+  
+  <style scoped>
+  .select-pokemon-page {
+      text-align: center;
+  }
+  
+  .select-pokemon-content {
+      text-align: left;
+      display: flex;
+      justify-content: space-evenly;
+  }
+  </style>
+  
