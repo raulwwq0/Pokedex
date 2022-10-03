6f8d0b931fe4ab47de4d88635d83e3f350d8b48b
Abhishek Juneja <abhishek.juneja145@gmail.com>
2022-10-03 15:53:59 +0530

    barebones homepage and favorites page with router config

---
+++ pokedex/src/pages/homePage.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="select-pokemon-page">
+        <img src="./../assets/logo-pixel.gif"
+        alt="pokémon"/>
+        <h1>Home Page</h1>
+        <div class="select-pokemon-content" />
+    </div>
+</template>
+
+<script>
+    export default {
+        async created() {
+            const pokemonData = await this.getPokemonData()
+            this.setPokemonDat(pokemonData)
+        },
+        methods: {
+            async getPokemonData() {
+                const data = await fetch(
+                    'https://pokeapi.co/api/v2/pokemon?limit=150'
+                )
+                const apiResult = await data.json()
+                return apiResult.results
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .select-pokemon-page {
+        text-align: center;
+    }
+
+    .select-pokemon-content {
+        text-align: left;
+        display: flex;
+        justify-content: space-evenly;
+    }
+</style>
--- pokedex/src/router/index.js
+++ pokedex/src/router/index.js
@@ -1,28 +1,24 @@
 import Vue from "vue";
-import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Router from "vue-router";
+// import HomeView from "../views/HomeView.vue";
+import home from "../pages/homePage.vue";
+import favorites from "../pages/favortiesPage.vue";
 
-Vue.use(VueRouter);
+Vue.use(Router);
 
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-];
-
-const router = new VueRouter({
-  routes,
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: home,
+    },
+    {
+      path: "/favorites",
+      name: "favorites",
+      component: favorites,
+    },
+  ],
 });
-
-export default router;
